464a3268419713103dff72d6033ad28b367cf7d5
Gerrit 'Geaz' Gazic <geaz@users.noreply.github.com>
2020-06-10 21:25:23 +0200

    Refactor empty MOD check

--- src/murli/states/read_mod_state.cpp
+++ src/murli/states/read_mod_state.cpp
@@ -1,10 +1,10 @@
 #ifndef READMODSTATE_H
 #define READMODSTATE_H
 
-#include "../state.hpp"
-#include "../murli_context.hpp"
 #include "run_mod_state.cpp"
 #include "invalid_mod_state.cpp"
+#include "../state.hpp"
+#include "../murli_context.hpp"
 #include "../../display/views/read_mod_view.cpp"
 #include "../../visualization/script_context.hpp"
 
@@ -26,26 +26,34 @@ namespace Murli
 
                 std::vector<uint8_t> modChars;
                 uint8_t result = context.getRom().read(modChars, ModMemorySize);
-                if(result != 0) context.currentState = std::make_shared<InvalidModState>();
-                else
-                {                        
-                    std::string loadedMod((char*)&modChars[0]);
-                    bool isEmptyMod = true;
-                    for (int i = 1; i < loadedMod.length(); i++) 
-                        if (loadedMod[i] != loadedMod[0])
-                            isEmptyMod = false; 
 
-                    if(isEmptyMod) context.currentState = std::make_shared<InvalidModState>();
-                    else
-                    {
-                        auto scriptContext = std::make_shared<ScriptContext>(loadedMod);                    
-                        if(!scriptContext->isFaulted()) context.currentState = std::make_shared<RunModState>(scriptContext);
-                        else context.currentState = std::make_shared<InvalidModState>();
-                    }
-                }             
+                if(result == 0 && !isEmptyMod(modChars))
+                {
+                    std::string loadedMod((char*)&modChars[0]);
+                    auto scriptContext = std::make_shared<ScriptContext>(loadedMod);                    
+                    if(!scriptContext->isFaulted()) context.currentState = std::make_shared<RunModState>(scriptContext);
+                    else context.currentState = std::make_shared<InvalidModState>();
+                }
+                else context.currentState = std::make_shared<InvalidModState>();            
             }
 
         private:
+            bool isEmptyMod(std::vector<uint8_t>& modChars)
+            {
+                // A factory ROM will return the same char for the
+                // whole memory - if the mod is empty, it is invalid
+                bool isEmptyMod = true;
+                for (int i = 1; i < modChars.size(); i++)
+                {
+                    if (modChars[i] != modChars[0])
+                    {
+                        isEmptyMod = false;
+                        break;
+                    }
+                }
+                return isEmptyMod;
+            }
+
             std::shared_ptr<ReadModView> _readModView;
     };
 }
